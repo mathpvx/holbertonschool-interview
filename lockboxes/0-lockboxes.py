e81fc26d2fb6b5382321db7a0917460198b43777
mathpvx <7547@holbertonstudents.com>
2024-11-26 10:20:02 +0100

    pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,12 +2,13 @@
 
 '''Method to determine if all the boxes can be opened'''
 
+
 def canUnlockAll(boxes):
     # sets the unlocked box to the index of the first unlocked box
     unlocked = set([0])
     # sets the keys to the first index of the boxes
     keys = set(boxes[0])
-    
+
     while keys:
         # takes one key from the set
         key = keys.pop()
@@ -16,6 +17,6 @@ def canUnlockAll(boxes):
             unlocked.add(key)
             # add new keys from the unlocked box
             keys.update(boxes[key])
-    
+
     # check if all boxes are unlocked
     return len(unlocked) == len(boxes)
