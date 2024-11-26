559a210396f37f99536960c52c976bc0f8f5640e
mathpvx <7547@holbertonstudents.com>
2024-11-26 11:00:25 +0100

    pep8

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-'''calculates the perimeter of the lands represented by 1 considering length of a 1 cell is 1'''
+'''
+Calculates the perimeter of the lands represented by
+1 considering length of a 1 cell is 1
+'''
+
 
 def island_perimeter(grid):
     perimeter = 0
@@ -14,10 +18,10 @@ def island_perimeter(grid):
                 if row > 0 and grid[row-1][column] == 1:
                     # Shared edge with the cell above
                     perimeter -= 2
-                
+
                 # Check the cell to the left
                 if column > 0 and grid[row][column-1] == 1:
                     # Shared edge with the cell to the left
-                    perimeter -= 2 
+                    perimeter -= 2
 
     return perimeter
