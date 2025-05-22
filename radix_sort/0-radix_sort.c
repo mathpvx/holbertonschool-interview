1cc09d4bb7144f1db335b5f3b15bf2a9b515fa9f
mathpvx <7547@holbertonstudents.com>
2025-05-22 15:22:38 +0200

    radix sort

---
+++ radix_sort/0-radix_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * get_max - Finds the maximum number in an array
+ * @array: Pointer to array
+ * @size: Size of array
+ * Return: Maximum number
+ */
+int get_max(int *array, size_t size)
+{
+	size_t i;
+	int max = array[0];
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+ * counting_sort_radix - Performs counting sort based on a digit
+ * @array: Pointer to array
+ * @size: Size of array
+ * @exp: Exponent (digit place)
+ */
+void counting_sort_radix(int *array, size_t size, int exp)
+{
+	int *output, count[10] = {0};
+	size_t i;
+
+	output = malloc(sizeof(int) * size);
+	if (!output)
+		return;
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
+
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = size; i > 0; i--)
+	{
+		output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
+		count[(array[i - 1] / exp) % 10]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+
+	print_array(array, size);
+	free(output);
+}
+
+/**
+ * radix_sort - Sorts array using LSD Radix Sort
+ * @array: Pointer to array
+ * @size: Size of array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max, exp;
+
+	if (!array || size < 2)
+		return;
+
+	max = get_max(array, size);
+
+	for (exp = 1; max / exp > 0; exp *= 10)
+		counting_sort_radix(array, size, exp);
+}
